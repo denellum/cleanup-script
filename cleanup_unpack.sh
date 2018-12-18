e11441371b192792cd67f04e7ab9f857261c7a08
denellum <trevor.harbin1987@gmail.com>
2018-12-18 13:47:23 -0600

    v_1.1
    Original Fixes, was late when created. - denellum

--- cleanup_unpack.sh
+++ cleanup_unpack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-########################################################
+#############################################################
 # CLEAN UP SCRIPT DESCRIPTION : 
-#########################################################
+#############################################################
 # This script will go through a defined LOCATION and
 # find directories with a ZIPPED file. When it finds a 
 # ZIPPED file it will set that subdirectory to an array
@@ -10,31 +10,32 @@
 # it's been moved. When those checks all are okay, the 
 # unpacked file will be deleted, assuming it's a 
 # duplicate at that point.
-#########################################################
+#############################################################
 
-#########################################################
+#############################################################
 # VERSIONING // CHANGELOG
-#########################################################
+#############################################################
 # v_1.0 - Initial Script. -denellum
-#########################################################
+# v_1.1 - Original Fixes, was late when created. - denellum
+#############################################################
 
-#########################################################
+#############################################################
 # VARBIABLES - You might been to change some of these.
-#########################################################
+#############################################################
 LOCATION="/mnt/user/Media/incomplete/"
 UNPACKED_FILES="*mkv"
 ZIPPED="*rar"
 LOCATION_WITH_ZIPPED=($(find "$LOCATION" -type f -name "$ZIPPED" | sed -r 's|/[^/]+$||' |sort |uniq))
 # Age is in minues.
 FILEAGE="360"
-#########################################################
+#############################################################
 
-#########################################################
-# ACUTAL SCRIPT
-#########################################################
+#############################################################
+# ACTUAL SCRIPT
+#############################################################
 
 for i in ${LOCATION_WITH_ZIPPED[@]};
 do
-     find $LOCATION -name "$ZIPPED" -type f -mmin +"$FILEAGE" -delete
+     find $i -name *"$UNPACKED_FILES" -type f -mmin +"$FILEAGE" -delete
 done
 
